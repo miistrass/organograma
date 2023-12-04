0de330765582635607972a72479d2cd9378bfc59
Milena Strassburger <milenastrass@gmail.com>
2023-12-04 06:30:37 -0300

    creating new react project

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/images/header.png" alt="Banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner
