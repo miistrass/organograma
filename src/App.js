7949fe38479459318696d542c7bcdb422b3ad0d1
Milena Strassburger <milenastrass@gmail.com>
2023-12-14 15:42:00 -0300

    adding set times and input color

--- src/App.js
+++ src/App.js
@@ -6,43 +6,43 @@ import Rodape from './components/Rodape/Rodape';
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#57C278',
+      
     },
     {
       nome: 'Front-end',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#82CFFA',
+      
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#A6D157',
+      
     },
     {
       nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#E06B69',
+      
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#DB6EBF',
+      
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFBA05',
+      
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FF8A29',
+      
     }
-  ]
+  ])
   
   const inicial = [
     {
@@ -194,7 +194,16 @@ function App() {
   const[colaboradores, setColaboradores] = useState(inicial)
 
   function deletarColaborador() {
+    console.log('deletando colaborador')
+  }
 
+  function mudarCorDoTime(cor, nome){
+    setTimes(times.map(time => {
+      if(time.nome === nome){
+        time.cor = cor
+      }
+      return time
+    }))
   }
   
   return (
@@ -206,7 +215,13 @@ function App() {
 
         <h1>Minha organização</h1>
 
-        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+        {times.map((time, indice) => 
+        <Time 
+          mudarCor={mudarCorDoTime}
+          key={indice} 
+          time={time} 
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
+          aoDeletar={deletarColaborador}/>)}
       </section>
 
       <Rodape />
--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,13 +1,23 @@
 import Colaborador from '../Colaborador/Colaborador'
 import './Time.css'
+import hexToRgba from 'hex-to-rgba';
 
-const Time = ({time, colaboradores, corSecundaria, aoDeletar}) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-        (colaboradores.length) > 0 && <section className='time' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <input value={time.corSecudaria} type='color' className='input-cor' />
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input 
+                onChange={evento => mudarCor(evento.target.value, time.nome)} 
+                value={time.cor} 
+                type='color' 
+                className='input-cor' 
+            />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={time.aoDeletar}/>)}
+                {colaboradores.map((colaborador, indice) => { 
+                
+                    return <Colaborador key={indice} colaborador={colaborador} 
+                    corDeFundo={time.cor} 
+                    aoDeletar={aoDeletar}/>})}
             </div>
         </section>
     )
